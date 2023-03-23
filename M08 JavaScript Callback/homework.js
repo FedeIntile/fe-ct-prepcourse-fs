6712975228dbb253b2e2b62bca88c887d272d2de
FedeIntile <122755209+FedeIntile@users.noreply.github.com>
2023-03-23 18:58:25 -0300

    completo hasta el modulo 8   23/03/2023

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -9,7 +9,7 @@ function mayuscula(nombre) {
    return nombre[0].toUpperCase() + nombre.slice(1); 
    // en la primera parte solo retorna la primera letra en mayuscula aplicando el metodo toUpperCase
    // luego se concatena usando el metodo slice(metodo array) que se usa pasando como paramentro la primera
-   // primera posicion de la cadena(como si fuera un array) y devuelve de la misma hst el final
+   // primera posicion de la cadena(como si fuera un array) y devuelve de la misma hasta el final
    //tambien se puede usar slice con 2 parametros ej.nombre.slice(2,3) y devuelve las posiciones
    // de la 2 hasta una antes de las 3
 }
@@ -18,6 +18,7 @@ function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   cb();
 }
 
 function operacionMatematica(num1, num2, cb) {
@@ -25,6 +26,17 @@ function operacionMatematica(num1, num2, cb) {
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
+   return cb(num1,num2); 
+
+   //lo que hace este callback ej con una suma
+
+   // function suma (num1,num2){
+   //    return num1 + num2; 
+   // }
+   
+   // let resultado = operacionMatematica(3,2,suma);
+   // console.log(resultado);   
+
 }
 
 function sumarArray(arrayOfNumbers, cb) {
@@ -33,6 +45,17 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   //opcion 1
+   // let suma = 0
+   // for(var i=0;i<arrayOfNumbers.length;i++){
+   //    suma = suma + arrayOfNumbers[i]; 
+   // }
+   // cb(suma);
+   //opcion 2: metodo reduce(metodo utilizado con array, puede juntar string o sumar numeros)
+   let suma = arrayOfNumbers.reduce((numero,aculudaror)=>{
+      return aculudaror + numero}
+   )
+   cb(suma); 
 }
 
 function forEach(array, cb) {
@@ -40,6 +63,19 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
+
+   // for(var i=0;i<array.length;i++){
+   //    cb(array[i])
+   // }
+   //opcion 2:metodo forEach(este metodo no devuelve nada)
+   array.forEach((numero) =>{
+      cb(numero); 
+   })
+   //opcion 3: metodo for in(devuelve el indice) y for of (devuelve el elemento)
+   // for (var i in array){
+   //    cb(array[i]); 
+   // }
+
 }
 
 function map(array, cb) {
@@ -47,13 +83,30 @@ function map(array, cb) {
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
+
+   //metodo map: este metodo de arreglos q devuelve un nuevo metodo modificado, pero no afecta al original
+   
+   // let nuevoArray=[]; 
+   // nuevoArray = array.map(elemento => cb(elemento));
+   // return nuevoArray; 
+
+   //opcion 2 : con forEach que no devuelve nada
+   let nuevoArray2=[];
+   array.forEach((e)=>nuevoArray2.push(cb(e))); /// el map los pushea a todos automaticamente
+   return nuevoArray2; 
 }
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   // el metodo filter devuelve un nuevo array que cumpla con alguna condicion que le indiquemos
+   let nuevoArray=[];
+   nuevoArray = arrayOfStrings.filter((e)=>e[0]==="a")
+   return nuevoArray; 
 }
+
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
